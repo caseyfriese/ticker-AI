d06f421a969bdbdd2267b5d4f23a3c1e07fc2849
Casey <griffter@gmail.com>
2020-05-17 21:46:27 -0500

    Initial Commit of MVC

---
+++ tickerAI/tickerAI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tickerAI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

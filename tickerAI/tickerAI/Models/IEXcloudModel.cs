7c086f13dfe30392ec02ca917af32b31f5608285
kaydenfriese <kayden.friese82203@gmail.com>
2020-05-26 11:56:57 -0500

    ---

---
+++ tickerAI/tickerAI/Models/IEXcloudModel.cs
@@ -0,0 +1,13 @@
+﻿namespace IEXcloudCharts.Models
+{
+    //public class IEXcloudModel
+    //{
+        //public string Symbol { get; set; }
+        //public string Date { get; set; }
+        //public decimal Open { get; set; }
+        //public decimal High { get; set; }
+        //public decimal Close { get; set; }
+        //public decimal Low { get; set; }
+        //public decimal Volume { get; set; }
+    //}
+}
